3def1875520a2da6dafb6410fa1ce9432e5a0069
Khalid Sookia <khalidsookia@gmail.com>
2015-07-03 07:31:57 +0200

    Added front end templates

--- src/Blog/AppBundle/BlogAppBundle.php
+++ src/Blog/AppBundle/BlogAppBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace Blog\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class BlogAppBundle extends Bundle
 {
 }
